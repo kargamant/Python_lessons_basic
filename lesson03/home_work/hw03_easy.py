42238f464d5c56ee2b5c773725df4e4e4e4cd6f7
kargamant <54020145+kargamant@users.noreply.github.com>
2019-08-26 13:58:10 +0300

    Update hw03_easy.py

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -1,3 +1,5 @@
+__author__ = "Дериглазов Егор Дмитриевич"
+
 # Задание-1:
 # Напишите функцию, округляющую полученное произвольное десятичное число
 # до кол-ва знаков (кол-во знаков передается вторым аргументом).
@@ -5,9 +7,8 @@
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
 def my_round(number, ndigits):
-    pass
-
-
+    result = round(number,ndigits)
+    return result
 print(my_round(2.1234567, 5))
 print(my_round(2.1999967, 5))
 print(my_round(2.9999967, 5))
@@ -20,9 +21,13 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
-
-
+       D = str(ticket_number)
+       beginning = int(D[:1] + D[1:2])
+       ending = int(D[-1] + D[-2])
+       if beginning == ending:
+           return True
+       else:
+           return False
 print(lucky_ticket(123006))
 print(lucky_ticket(12321))
 print(lucky_ticket(436751))
